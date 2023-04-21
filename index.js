c228ceea3db235a03e918bda853589a9b9e16d60
Aneya Seignious <nseignious@gmail.com>
2023-04-20 20:19:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+const cards = (["Guadalupe", "Ollie", "Aki"], "surprise");
+function writeCards(cards, event) {
+    const outputArray = []
+    for (let c = 0; c < cards.length; c++) {
+        outputArray.push(`Thank you, ${cards[c]}, for the wonderful ${event} gift!`);
+        
+    }
+    return outputArray 
+}
+function countDown (number) {
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
